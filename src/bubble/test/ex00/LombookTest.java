79381f22424832919a775c76bd25430cddf1bc56
832552 <jihoon.oh@edu.sait.ca>
2022-04-25 10:15:55 -0600

    bubble swing java game

---
+++ src/bubble/test/ex00/LombookTest.java
@@ -0,0 +1,23 @@
+package bubble.test.ex00;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class Dog{
+	 private String name;
+ 
+}
+
+
+public class LombookTest {
+ 
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.setName("testDog");
+		System.out.println(d.getName());
+		
+	}
+
+}
