52501f26e3e92b06d3120d78a15d2264994bfe44
jihoon <jihoon.oh@edu.sait.ca>
2022-04-27 22:36:26 -0600

    bubble

---
+++ src/bubble/test/ex04/Bubble.java
@@ -0,0 +1,53 @@
+package bubble.test.ex04;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel {
+	private int x;
+	private int y;
+
+	private Player player;
+
+	private boolean left;
+	private boolean right;
+	private boolean up;
+
+// 적군을 맞춘 상태 
+	private int state; // 0(물방울) 1 ( 적을 가둔 물방울)
+	private ImageIcon bubble; // 물방울
+	private ImageIcon bubbled; // 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진 상태
+
+	public Bubble(Player player) {
+		this.player = player;
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		bubble = new ImageIcon("Image/bubble.png");
+		bubbled = new ImageIcon("Image/bubbled.png");
+		bomb = new ImageIcon("Image/bomb.png");
+
+	}
+
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+		x = player.getX();
+		y = player.getY();
+		
+		setIcon(bubble);
+		setSize(50,50);
+		state =0;
+
+	}
+
+}
